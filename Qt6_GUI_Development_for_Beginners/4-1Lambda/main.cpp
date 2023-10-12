e8fa7fc7ce538cfd12520986889ec49660df3d65
SolarAA-11 <948872263@qq.com>
2023-10-12 23:22:10 +0800

    QWidget Signal and Slot

---
+++ Qt6_GUI_Development_for_Beginners/4-1Lambda/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+//    auto func = []() {
+//        cout << "Hello World!" << endl;
+//    };
+//    func();
+
+    // Call lambda function immediately after definition
+//    [](){
+//        cout << "Hello World!" << endl;
+//    }();
+
+    // Define a lambda function that takes some parameters
+//    [](int a, int b) {
+//        cout << a + b << endl;
+//    }(7, 3);
+
+    // Store a lambda variable
+//    auto func = [](int a, int b) {
+//        cout << a + b << endl;
+//    };
+//    func(1, 2);
+
+    // Define a lambda that returns a value
+//    auto func = [](int a, int b) -> int {
+//        return a + b;
+//    };
+//    auto value = func(1, 2);
+//    cout << value << endl;
+
+
+
+
+    return 0;
+}
