12a80c5f12f9d128a779df8299b36286e2fb8c68
SolarAA-11 <948872263@qq.com>
2023-11-01 09:40:33 +0800

    Learn QLabel

--- LearnWidgets/LearnQLabel/widget.cpp
+++ LearnWidgets/LearnQLabel/widget.cpp
@@ -1,11 +1,27 @@
 #include "widget.h"
 #include "ui_widget.h"
 
+#include <QPixmap>
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    // Set label content
+    ui->plainTextLabel->setText("Plain Text");
+    ui->richTextLabel->setText("<h1>Rich Text</h1>");
+
+    // Set Pixmap
+    QPixmap pixmap("://resource/a.png");
+    ui->pixmapLabel->setPixmap(pixmap);
+
+    ui->numberLabel->setNum(12);
+
+
+    // Label Alignment
+    ui->baselineLabel->setAlignment(Qt::AlignBaseline);
 }
 
 Widget::~Widget()
