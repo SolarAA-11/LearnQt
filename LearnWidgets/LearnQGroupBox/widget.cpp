0f941d51f0c8a6d5b727fabcfb8835beca036013
SolarAA-11 <948872263@qq.com>
2023-11-02 11:58:43 +0800

    Learn QGroupBox and QWidget

--- LearnWidgets/LearnQGroupBox/widget.cpp
+++ LearnWidgets/LearnQGroupBox/widget.cpp
@@ -1,19 +1,11 @@
 #include "widget.h"
 #include "ui_widget.h"
 
-#include <QPixmap>
-#include <QIcon>
-
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
-
-    setWindowTitle("Hello World");
-
-    QPixmap pixmap("://images/a.png");
-    setWindowIcon(QIcon(pixmap));
 }
 
 Widget::~Widget()
